a9f334f2adb30dbc3976fff5dc3c961a9ea4a756
Donut <nutchanokchon5@gmail.com>
2020-10-13 16:01:05 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const selectionButtons = document.querySelectorAll('[data-selection]')
+const finalColumn = document.querySelector('[data-final-column]')
+const computerScoreSpan = document.querySelector('[data-computer-score]')
+const yourScoreSpan = document.querySelector('[data-your-score]')
+const SELECTTIONS = [
+    {
+        name: 'rock',
+        emoji: '✊',
+       beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋' ,
+        beats: 'rock' 
+    },
+    {
+        name: 'scissors',
+        emoji: '✌',
+        beats: 'paper' 
+    }
+]
+
+
+// to make the button store the value as user click IDK LOL
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener('click', e => {
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
+    })
+})
+
+function makeSelection(selection){
+    const computerSelection = randomSelection()
+    const yourWinner =  isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+   
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+
+    if (yourWinner)incrementScore(yourScoreSpan) 
+    if (computerWinner)incrementScore(computerScoreSpan)  
+}
+
+function incrementScore(scoreSpan) {
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+}
+
+
+function addSelectionResult(selection, winner) {
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+
+
+
+function randomSelection(){
+    const randomIndex = Math.floor(Math.random() * SELECTTIONS.length)
+    return SELECTTIONS[randomIndex]
+}
+
